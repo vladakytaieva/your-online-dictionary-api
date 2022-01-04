ca8bf83d34ea5a3d656b95e9406582d8856264e8
unknown <vlada.kitaeva2002@gmail.com>
2022-01-04 17:54:11 +0200

    adding db

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const user = require("./controllers/user")
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'postgres',
-      password : 'potato',
-      database : 'wreview'
+      connectionString: process.env.DATABASE_URL,
+      ssl: true
     }
 })
 
